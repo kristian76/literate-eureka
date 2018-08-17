0a3c36275403b15db4af90e9dcdd9085211c8f65
Kristian Nissen <kristian.nissen@gmail.com>
2018-08-17 12:07:35 +0000

    Webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+/**
+ * Webpack Config
+ */
+var path = require('path'),
+    webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        project: './src/index.js'
+    },
+    output: {
+        path: path.join(__dirname, 'dist', 'js'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
+};
