aad0934b9de490a624cd3a38573acd8d486b8d1e
Kristian Nissen <kristian.nissen@gmail.com>
2018-08-17 12:13:00 +0000

    Prep for PWA

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('Hello Kitty')
+/** PWA stuff
+ * Resources https://developers.google.com/web/fundamentals/codelabs/your-first-pwapp/
+**/
+if ('serviceWorker' in navigator) {
+    // Register serviceWorker
+    navigator.serviceWorker
+        .register('./service-worker.js')
+        .then(function() {
+            console.log('service worker registered')
+        })
+}
